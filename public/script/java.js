984bae681531507d9f36d67cbfa69cf151f7c806
bhseo1223 <bhseo1223@gmail.com>
2023-01-04 17:40:33 +0900

    1st commit

--- public/script/java.js
+++ public/script/java.js
@@ -1,8 +1,24 @@
 /* bhseo1223 nodejs : script - java : rkmarket_app */
 
-function heightSizeChange(id, value) { /* height 변경 */
-    document.getElementById(id).style.height = value;
-}
+function heightSizeChange(id1, id2) { /* height 변경 */
+    if (document.getElementById(id1).style.height == '28vw') {
+        document.getElementsByClassName('points-point')[0].style.height = '28vw';
+        document.getElementsByClassName('points-point')[1].style.height = '28vw';
+        document.getElementsByClassName('points-point')[2].style.height = '28vw';
+        document.getElementsByClassName('points-point')[3].style.height = '28vw';
+        document.getElementsByClassName('points-point')[4].style.height = '28vw';
+        document.getElementsByClassName('point-detail')[0].style.visibility = 'hidden';
+        document.getElementsByClassName('point-detail')[1].style.visibility = 'hidden';
+        document.getElementsByClassName('point-detail')[2].style.visibility = 'hidden';
+        document.getElementsByClassName('point-detail')[3].style.visibility = 'hidden';
+        document.getElementsByClassName('point-detail')[4].style.visibility = 'hidden';
+        document.getElementById(id1).style.height = '48vw';
+        document.getElementById(id2).style.visibility = 'visible';
+    } else {
+        document.getElementById(id1).style.height = '28vw';
+        document.getElementById(id2).style.visibility = 'hidden';
+    };
+};
 
 
 /* bhseo1223 nodejs : script - java : rkmarket_app */
